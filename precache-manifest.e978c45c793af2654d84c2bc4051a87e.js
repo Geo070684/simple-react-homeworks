fa88418ea46f89000c9343e6f5796dfd1b0d1569
George Erokhin <Georgii070684@gmail.com>
2022-09-15 15:49:13 +0300

    Updates

---
+++ precache-manifest.e978c45c793af2654d84c2bc4051a87e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23f3c843f5dd1b1756a7bfe25b1a156e",
+    "url": "/simple-react-homeworks/index.html"
+  },
+  {
+    "revision": "4ff5a98e48e4e20310cc",
+    "url": "/simple-react-homeworks/static/css/main.99abc8d7.chunk.css"
+  },
+  {
+    "revision": "7276d8d6083b59ecf520",
+    "url": "/simple-react-homeworks/static/js/2.885fba6c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/simple-react-homeworks/static/js/2.885fba6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ff5a98e48e4e20310cc",
+    "url": "/simple-react-homeworks/static/js/main.c6043760.chunk.js"
+  },
+  {
+    "revision": "82c3ed739719c6917bd6",
+    "url": "/simple-react-homeworks/static/js/runtime-main.310d1191.js"
+  }
+]);
